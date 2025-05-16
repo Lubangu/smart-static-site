0c29fd3c630fa6d7b3569a3633ee4f7a993b8376
Lubangu <Lubangu@users.noreply.github.com>
2025-05-16 18:13:54 +0100

    Initial commit for smart static site

--- lib/smart-static-site-stack.ts
+++ lib/smart-static-site-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class SmartStaticSiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'SmartStaticSiteQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const siteBucket = new s3.Bucket(this, 'SiteBucket', {
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'error.html',
+      publicReadAccess: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
   }
 }
